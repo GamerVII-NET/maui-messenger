81cc3c862108e38027e57bf396b21e2fa094240b
MAUI Messenger <maui-messenger@internet.ru>
2022-08-14 13:43:35 +0300

    Initial commit

---
+++ Messenger.Client/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Messenger.Client
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
