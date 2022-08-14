b028f8a1d19cab48ac328b9760c401b22d6b2e55
MAUI Messenger <maui-messenger@internet.ru>
2022-08-14 14:05:02 +0300

    initializing MVVM

---
+++ Messenger.Client/ViewModels/WelcomePageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger.Client.ViewModels
+{
+    public partial class WelcomePageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        string _name;
+
+        public WelcomePageViewModel()
+        {
+            _name = "MAUI Messenger";
+        }
+
+
+    }
+}
---
+++ Messenger.Client/Views/Pages/WelcomePageViewModel.xaml.cs
@@ -0,0 +1,9 @@
+namespace Messenger.Client.Views.Pages;
+
+public partial class WelcomePageViewModel : ContentPage
+{
+	public WelcomePageViewModel()
+	{
+		InitializeComponent();
+	}
+}
